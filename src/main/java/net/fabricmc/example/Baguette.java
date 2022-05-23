b591a9f8a197f2a09627634392bd344fc1b71654
camilleperrin26 <camilleperrin26@gmail.com>
2022-05-23 09:36:46 +0200

    c

--- src/main/java/net/fabricmc/example/Baguette.java
+++ src/main/java/net/fabricmc/example/Baguette.java
@@ -2,18 +2,26 @@ package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Baguette extends Item implements ModInitializer {
+public class Baguette extends Item implements ModInitializer  {
 
 	//public static ItemConvertible Baguette;
 	public static final Item Baguette = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final Logger LOGGER = LoggerFactory.getLogger("Baguette");
+	//Item texture: /resources/assets/Items/Objects/Baguette.png
 
 	public Baguette(Settings settings)
 	{
@@ -21,12 +29,20 @@ public class Baguette extends Item implements ModInitializer {
 	}
 
 	@Override
+	//Se lance quand utilisé par un joueur
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+		playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
+		//Mettre ici l'ouverture de l'HUD
+		return TypedActionResult.success(playerEntity.getStackInHand(hand));
+	}
+
+	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registry.ITEM, new Identifier("tutorial", "custom_item"), Baguette);
+		Registry.register(Registry.ITEM, new Identifier("Baguette", "custom_item"), Baguette);
 		//LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Baguette launched");
 	}
-
 }

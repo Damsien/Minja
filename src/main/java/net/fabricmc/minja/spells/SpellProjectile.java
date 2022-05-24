e0ae5aa78a86a69e326255887539a642b6078194
Arnaud <arnaud.enseeiht@gmail.com>
2022-05-24 13:53:24 +0200

    LightningBall working (no gravity with wand)

--- src/main/java/net/fabricmc/minja/spells/SpellProjectile.java
+++ src/main/java/net/fabricmc/minja/spells/SpellProjectile.java
@@ -7,25 +7,10 @@ public abstract class SpellProjectile extends Spell {
 
     public ProjectileEntity projectile;
 
-    //List of Minja projectile's spells
-    //CAMILLE : NE PAS SUPPRIMER
-    //public static final SpellProjectile lightningball = new SpellProjectile("Lightning Ball", 1, "xxx", "basic", getProjectile());
-
-
     public SpellProjectile(String name, int manaCost, String icon, String type, ProjectileEntity projectile) {
         super(name, manaCost, icon, type);
         this.projectile = projectile;
     }
-
-    //CAMILLE : NE PAS SUPPRIMER
-    /*public static SpellProjectile getSpell(String spell) {
-        switch (spell){
-            case "lightningball" :
-                return lightningball;
-            default :
-                return null;
-        }
-    }*/
 
     public ProjectileEntity getProjectile() {
         return projectile;

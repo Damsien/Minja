fca17bc5068c0be468b5d62b4178cc3c334cbef8
anais.calza <anais.calza@gmail.com>
2022-05-24 12:28:27 +0200

    ingamehudmixin update comments

--- src/main/java/net/fabricmc/minja/mixin/InGameHudMixin.java
+++ src/main/java/net/fabricmc/minja/mixin/InGameHudMixin.java
@@ -31,30 +31,35 @@ public class InGameHudMixin extends DrawableHelper {
 
     @Inject(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 1))
     private void renderStatusBarsMixin(MatrixStack matrices, CallbackInfo info) {
-        PlayerEntity playerEntity = this.getCameraPlayer();
+        PlayerEntity playerEntity = this.getCameraPlayer(); // get the current player
         if (playerEntity != null) {
+            int currentMana = ((PlayerMinja) playerEntity).getMana(); // get the mana of the player
             int manaMax = ((PlayerMinja) playerEntity).getManaMax();
-            LivingEntity livingEntity = this.getRiddenEntity();
+            LivingEntity livingEntity = this.getRiddenEntity(); // test if player is riding an entity
+
             int variable_one;
             int variable_two;
-            int height = this.scaledHeight - 49;
-            int width = this.scaledWidth / 2 + 91;
-            if (this.getHeartCount(livingEntity) == 0) {
-                for (variable_one = 0; variable_one < 10; ++variable_one) {
-                    int uppderCoord = 9;
-                    /**if (ConfigInit.CONFIG.other_droplet_texture) {
-                     uppderCoord = uppderCoord + 9;
-                     }*/
-                    int beneathCoord = 0;
+            int height = this.scaledHeight - 49; // why theses values ???
+            int width = this.scaledWidth / 2 + 91; // why theses values ???
 
+            if (this.getHeartCount(livingEntity) == 0) { // (TODO : Move) (currently) Hide the mana bar if the player is riding
+                for (variable_one = 0; variable_one < 10; ++variable_one) { // why ?????????
+                    int uppderCoord = 9;
+                    int beneathCoord = 0;
                     variable_two = width - variable_one * 8 - 9;
+
+                    // get the texture of the mana
                     RenderSystem.setShaderTexture(0, MANA_ICON);
+                    // draw the texture placeholder (black one)
                     this.drawTexture(matrices, variable_two, height, 0, 0, 9, 9);
-                    if (variable_one * 2 + 1 < manaMax) {
-                        this.drawTexture(matrices, variable_two, height, beneathCoord, uppderCoord, 9, 9); // Big icon
+
+                    if (variable_one * 2 + 1 < currentMana) {
+                        // draw the big blue mana icon
+                        this.drawTexture(matrices, variable_two, height, beneathCoord, uppderCoord, 9, 9);
                     }
-                    if (variable_one * 2 + 1 == manaMax) {
-                        this.drawTexture(matrices, variable_two, height, beneathCoord + 9, uppderCoord, 9, 9); // Small icon
+                    if (variable_one * 2 + 1 == currentMana) {
+                        // draw the little blue mana icon
+                        this.drawTexture(matrices, variable_two, height, beneathCoord + 9, uppderCoord, 9, 9);
                     }
                 }
                 RenderSystem.setShaderTexture(0, GUI_ICONS_TEXTURE);
@@ -62,11 +67,23 @@ public class InGameHudMixin extends DrawableHelper {
         }
     }
 
-    @Inject(method = "getHeartRows", at = @At(value = "HEAD"), cancellable = true)
-    private void getHeartRowsMixin(int heartCount, CallbackInfoReturnable<Integer> info) {
-        info.setReturnValue((int) Math.ceil((double) heartCount / 10.0D) + 1);
+/**    private void showManaBar(int manaMax, int currentMana, MatrixStack matrices, int height) {
+        for (variable_one = 0; variable_one < 10; ++variable_one) {
+            int uppderCoord = 9;
+            int beneathCoord = 0;
+            variable_two = width - variable_one * 8 - 9;
+            RenderSystem.setShaderTexture(0, MANA_ICON);
+            this.drawTexture(matrices, variable_two, height, 0, 0, 9, 9);
+            if (variable_one * 2 + 1 < manaMax) {
+                this.drawTexture(matrices, variable_two, height, beneathCoord, uppderCoord, 9, 9); // Big icon
+            }
+            if (variable_one * 2 + 1 == manaMax) {
+                this.drawTexture(matrices, variable_two, height, beneathCoord + 9, uppderCoord, 9, 9); // Small icon
+            }
+        }
+        RenderSystem.setShaderTexture(0, GUI_ICONS_TEXTURE);
     }
-
+*/
     @Shadow
     private PlayerEntity getCameraPlayer() {
         return null;

c15ae455c42677e627dd154c5bed0f2ce5189a29
anais.calza <anais.calza@gmail.com>
2022-05-24 08:36:24 +0200

    Add missing file

--- src/main/java/net/fabricmc/minja/mixin/InGameHudMixin.java
+++ src/main/java/net/fabricmc/minja/mixin/InGameHudMixin.java
@@ -3,14 +3,19 @@ package net.fabricmc.minja.mixin;
 import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin extends DrawableHelper {
 
     private final MinecraftClient client;
+    private static final Identifier MANA_ICON = new Identifier("");
 
     public InGameHudMixin(MinecraftClient client) {
         this.client = client;
     }
+
+
+
 }

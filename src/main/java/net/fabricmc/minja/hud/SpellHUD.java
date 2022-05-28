49e3cd21cd0c8835a8690c2f6a78a1a46243488e
BuildTools <unconfigured@null.spigotmc.org>
2022-05-28 11:18:08 +0200

    Push current modification

--- src/main/java/net/fabricmc/minja/hud/SpellHUD.java
+++ src/main/java/net/fabricmc/minja/hud/SpellHUD.java
@@ -3,27 +3,40 @@ package net.fabricmc.minja.hud;
 
 import net.fabricmc.minja.Minja;
 import net.fabricmc.minja.PlayerMinja;
+import net.fabricmc.minja.spells.LightningBallItem;
 import net.fabricmc.minja.spells.Spell;
+import net.fabricmc.minja.spells.SpellProjectile;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 
 import java.awt.*;
 import java.util.ArrayList;
 
+@Mixin(InGameHud.class)
 public class SpellHUD {
 
     private MinecraftClient minecraft;
     private WorldRenderer worldRenderer;
     private TextRenderer fontRenderer;
+
+    private ItemRenderer itemRenderer;
     private PlayerMinja player;
 
     private static PointCartesien centre;
@@ -35,23 +48,24 @@ public class SpellHUD {
 
     private int width;
     private int height;
+
+
     public SpellHUD() {
 
         minecraft = MinecraftClient.getInstance();
         worldRenderer = minecraft.worldRenderer;
         fontRenderer = minecraft.textRenderer;
+        itemRenderer = minecraft.getItemRenderer();
         player = (PlayerMinja) minecraft.player;
         centre = new PointCartesien(minecraft.mouse.getX(),minecraft.mouse.getY());
 
         visible = false;
         isCenterSet = false;
-        initialize();
     }
 
-    public void initialize() {
-        Window mainWindow = MinecraftClient.getInstance().getWindow();
-        width = mainWindow.getWidth() / 4;
-        height = mainWindow.getHeight() / 4;
+    public void updateScreenDimension(int width, int height) {
+        this.width = width/2;
+        this.height = height/2;
     }
 
     public void updateCenter() {
@@ -63,6 +77,8 @@ public class SpellHUD {
     }
 
     public void onRenderGameOverlayPost(MatrixStack stack, float partialTicks) {
+
+        //updateScreenPosition(minecraft.player);
 
         final double LINE = (height * 0.7); // length between the center and each spells
         final double SAFE_RANGE = (height * 0.3); // length between the center and the crosshair in which no selection will be done
@@ -80,7 +96,6 @@ public class SpellHUD {
             this.updateCenter();
             isCenterSet = true;
         }
-
 
         // Get the position of the mouse
         PointCartesien mousePos = new PointCartesien(minecraft.mouse.getX(), minecraft.mouse.getY());
@@ -131,22 +146,14 @@ public class SpellHUD {
 
                 // Check if the current spell drawn is the active one
                 if(i == currentIndex)   draw(stack, i, width+p.x, height +p.y, Color.BLUE);
-                else                    draw(stack, i, width+p.x, height +p.y, Color.GREEN);
+                else                    draw(LightningBallItem.getLightningBall().asItem(),width+p.x,height +p.y);
 
 
             // In case we are in the selection zone :
             } else {
 
-                // Calculate the range of selection of the iterating spell
-                double borneInf = modulo(pp.theta - rad / 2, 2*Math.PI);
-                double borneSup = modulo(pp.theta + rad / 2, 2*Math.PI);
-
-                // Check if the mouse is in the range of the iterating spell
-                boolean isInInterval = i != 0   ? borneInf < thetaModulo && thetaModulo < borneSup                                                   // Classic case
-                                                : (borneInf < thetaModulo && thetaModulo < 2*Math.PI) || 0 < thetaModulo && thetaModulo < borneSup; //  Case 0 : borneSup < borneInf
-
-                // If this is the case :
-                if(isInInterval) {
+                // If the mouse is in the range of the current point :
+                if(isInInterval(pp,i,rad,thetaModulo)) {
                     // In case we have changed of interval, trigger a sound and update status
                     if(currentIndex != i) {
                         currentIndex = i;
@@ -156,7 +163,7 @@ public class SpellHUD {
                     draw(stack, i, width+p.x, height +p.y, Color.BLUE);
                 }
                 else
-                    draw(stack, i, width+p.x, height +p.y, Color.GREEN);
+                    draw(LightningBallItem.getLightningBall().asItem(),width+p.x,height +p.y);
 
             }
         }
@@ -169,27 +176,33 @@ public class SpellHUD {
         return result;
     }
 
+    private boolean isInInterval(PointPolaire vertex, int vertexIndex, double angleMoyen, double angleDeLaSouris) {
+        // Calculate the range of selection of the iterating spell
+        double borneInf = modulo(vertex.theta - angleMoyen / 2, 2*Math.PI);
+        double borneSup = modulo(vertex.theta + angleMoyen / 2, 2*Math.PI);
+
+        // Check if the mouse is in the range of the iterating spell
+        return vertexIndex != 0     ? borneInf < angleDeLaSouris && angleDeLaSouris < borneSup                                                   // Classic case
+                                    : (borneInf < angleDeLaSouris && angleDeLaSouris < 2*Math.PI) || 0 < angleDeLaSouris && angleDeLaSouris < borneSup; //  Case 0 : borneSup < borneInf
+    }
+
+
+
     private void draw(MatrixStack stack, int text, double x, double y, Color color) {
         draw(stack, ""+text, x, y, color);
     }
-
 
     private void draw(MatrixStack stack, String text, double x, double y, Color color) {
         fontRenderer.draw(stack, text, (float)x, (float)y, color.getRGB());
     }
 
-
-    public void setVisibility(boolean b) {
-        this.visible = b;
+    private void draw(Item item, double x, double y) {
+        itemRenderer.renderGuiItemIcon(item.getDefaultStack(), (int)x, (int)y);
     }
 
-    public static void toggleVisibility() {
-        System.out.println("Petit message de test");
-        visible = !visible;
-    }
 
-    public void afterRenderStatusEffects(MatrixStack stack, float partialTicks) {
-    }
+
+
 
 
     class PointPolaire {
--- src/main/java/net/fabricmc/minja/mixin/SpellHudMixin.java
+++ src/main/java/net/fabricmc/minja/mixin/SpellHudMixin.java
@@ -4,6 +4,7 @@ import net.fabricmc.minja.hud.SpellHUD;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,20 +13,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public abstract class SpellHudMixin {
 
+    @Shadow private int scaledWidth;
+    @Shadow private int scaledHeight;
+
     private static SpellHUD gui;
 
     @Inject(method = "renderStatusEffectOverlay", at = @At("RETURN"))
     private void afterRenderStatusEffects(MatrixStack stack, CallbackInfo ci) {
-        if (gui == null)
-            gui = new SpellHUD();
-        gui.afterRenderStatusEffects(stack, 0);
+        if (gui == null) gui = new SpellHUD();
     }
 
     @Inject(method = "render", at = @At("RETURN"))
 
     private void beforeRenderDebugScreen(MatrixStack stack, float f, CallbackInfo ci) {
-        if (gui==null)
-            gui=new SpellHUD();
+        if (gui==null)  gui=new SpellHUD();
+        gui.updateScreenDimension(scaledWidth, scaledHeight);
         gui.onRenderGameOverlayPost(stack, 0);
     }
 

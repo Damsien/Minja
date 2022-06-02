b373267929bf2ccc17ff8982aad26597a205b052
anais.calza <anais.calza@gmail.com>
2022-06-02 08:25:55 +0200

    Grimoire screen, gui and methods in grimoire (item)

--- src/main/java/net/fabricmc/minja/gui/GrimoireScreen.java
+++ src/main/java/net/fabricmc/minja/gui/GrimoireScreen.java
@@ -1,15 +1,13 @@
 package net.fabricmc.minja.gui;
 
 
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+import net.minecraft.client.MinecraftClient;
 
-public class GrimoireScreen extends Screen {
+public class GrimoireScreen extends CottonClientScreen {
 
-
-    protected GrimoireScreen(Text title) {
-        super(title);
+    public GrimoireScreen(GuiDescription description) {
+        super(description);
     }
-
-
 }

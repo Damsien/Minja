5f57d82ce4e24a62d266cf56662a53c6529097a8
BuildTools <unconfigured@null.spigotmc.org>
2022-06-03 08:38:25 +0200

    Add Side mechanism to identify if a request is from a server or from a client

--- src/main/java/net/fabricmc/minja/objects/Wand.java
+++ src/main/java/net/fabricmc/minja/objects/Wand.java
@@ -1,90 +1,28 @@
 package net.fabricmc.minja.objects;
 
-import net.fabricmc.minja.events.MinjaEvent;
-import net.fabricmc.minja.events.PlayerEvent;
-import net.fabricmc.minja.events.Side;
-import net.fabricmc.minja.exceptions.NotEnoughtManaException;
-import net.fabricmc.minja.PlayerMinja;
-import net.fabricmc.minja.spells.LightningBall;
-import net.fabricmc.minja.spells.Spell;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.StackReference;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.screen.slot.Slot;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.ClickType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
+		import net.fabricmc.minja.events.MinjaEvent;
+		import net.fabricmc.minja.events.Side;
+		import net.fabricmc.minja.exceptions.NotEnoughtManaException;
+		import net.fabricmc.minja.PlayerMinja;
+		import net.fabricmc.minja.hud.SpellHUD;
+		import net.minecraft.entity.player.PlayerEntity;
+		import net.minecraft.item.Item;
+		import net.minecraft.item.ItemStack;
+		import net.minecraft.text.LiteralText;
+		import net.minecraft.util.Hand;
+		import net.minecraft.util.TypedActionResult;
+		import net.minecraft.world.World;
 
-/*
- * Wand is the class that creates a Wand object and defines its interactions to the minecraft world.
- * The Wand will be used to throw spells (left-click) and to choose a current spell (right-click)
- */
 public class Wand extends MinjaItem {
-
-	/*********************************************************************
-	 * 						GENERAL (Constructor + getter)
-	 ********************************************************************* */
-
-	/*
-	 * The class Wand creates an object Wand itself.
-	 * We should have only one Wand object : no other class creates a Wand object.
-	 */
 	private static Wand WAND;
 
-	/*
-	 * Constructor of Wand, using the constructor of Item.
-	 */
 	public Wand(Settings settings) {
 		super(settings);
 		WAND = this;
 	}
 
-	/*
-	 * A getter to access the Wand object
-	 */
-	public static Item getWand() {
+	public static Item getWand(){
 		return WAND;
-	}
-
-	/*********************************************************************
-	 * 						WORLD INTERACTIONS
-	 ********************************************************************* */
-
-	/*
-	 * This method is used when the player use right click with the Wand
-	 */
-	/*@Override
-	public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-
-		// Get the current player
-		PlayerMinja player = (PlayerMinja) playerEntity;
-
-		try {
-			// Remove the current spell's mana cost to the mana of the player
-			((PlayerMinja) playerEntity).removeMana(player.getActiveSpell().getManaCost());
-
-			// Cast the spell associated
-			Spell spell = player.getActiveSpell();
-			spell.cast(playerEntity);
-
-		} catch (NotEnoughtManaException e) {
-			// throw new RuntimeException(e);
-			// TODO : A UPGRADE
-		}
-		return TypedActionResult.success(playerEntity.getStackInHand(hand));
-	}*/
-
-	@Override
-	public TypedActionResult<ItemStack> onRightClickPressed(World world, PlayerEntity playerEntity, Hand hand, Side side) {
-
-		playerEntity.sendMessage(new LiteralText("Event right : " + (side == Side.CLIENT ? "CLIENT" : "SERVER")), false);
-
-		return TypedActionResult.success(playerEntity.getStackInHand(hand));
 	}
 
 
@@ -93,11 +31,8 @@ public class Wand extends MinjaItem {
 
 		PlayerMinja player = (PlayerMinja) playerEntity;
 
-		playerEntity.sendMessage(new LiteralText("Event left : "+ (side == Side.CLIENT ? "CLIENT" : "SERVER")), false);
-
 		if(Side.CLIENT == side) {
-
-
+			playerEntity.sendMessage(new LiteralText(player.getActiveSpell().getName()),true);
 			try {
 				player.removeMana(player.getActiveSpell().getManaCost());
 			} catch (NotEnoughtManaException e) {
@@ -108,7 +43,7 @@ public class Wand extends MinjaItem {
 		}
 
 		if(Side.SERVER == side) {
-			if(player.getMana() < player.getActiveSpell().getManaCost())
+			playerEntity.sendMessage(new LiteralText(player.getActiveSpell().getName()),false);
 			player.getActiveSpell().cast(playerEntity);
 		}
 
@@ -117,15 +52,31 @@ public class Wand extends MinjaItem {
 		return MinjaEvent.SUCCEED;
 	}
 
-
-
-	/*
-	 * A EXPLIQUER !!! QUELQU'UN ????
-	 * TODO : A EXPLIQUER
-	 */
 	@Override
-	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-		((PlayerEntity)user).sendMessage(new LiteralText("Event de fin !!"), false);
-		return stack;
+	public TypedActionResult<ItemStack> onRightClickPressed(World world, PlayerEntity playerEntity, Hand hand, Side side) {
+
+		SpellHUD.setVisible(true);
+
+		return TypedActionResult.success(playerEntity.getStackInHand(hand));
+
 	}
+
+	@Override
+	public TypedActionResult<ItemStack> onRightClickMaintained(World world, PlayerEntity playerEntity, Hand hand, Side side) {
+
+		// Cancel Event ==> Cancel hand animation
+		return TypedActionResult.pass(playerEntity.getStackInHand(hand));
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> onRightClickReleased(World world, PlayerEntity playerEntity, Hand hand, Side side) {
+		//Mettre ici l'ouverture de l'HUD
+		SpellHUD.setVisible(false);
+
+		return null;
+	}
+
+
+
 }
+
